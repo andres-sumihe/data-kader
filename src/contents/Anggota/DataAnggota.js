172ec7b0402795acebe5761fcde25b94a7631ba6
riettsruff <riettsruff@gmail.com>
2020-03-21 18:39:19 +0700

    Update...

--- src/contents/Anggota/DataAnggota.js
+++ src/contents/Anggota/DataAnggota.js
@@ -8,15 +8,108 @@ class DataAnggota extends React.Component {
 		return (
 			<div className="DataAnggota">
 				<div className="HeadDataAnggota">
-					<div className="ResultData BroderGray"></div>
+					<div className="ResultData">
+						<span>Menampilkan <span className="bold">9</span> Data</span>
+					</div>
 					<div className="DisplayDataBy">
-						<div className="ByGrid">
-							<FontAwesomeIcon icon={}
+						<div className="ByGrid Active">
+							<FontAwesomeIcon icon={faTh} />
 						</div>
-						<div className="ByList"></div>
+						<div className="ByList">
+							<FontAwesomeIcon icon={faList} />
+						</div>	
 					</div>
 				</div>
-				<div className="BodyDataAnggota"></div>
+				<div className="BodyDataAnggota row">
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo1.jpg")})` }}>
+							</div>
+							<div className="Nama">Kimono Budi Fuzzy</div>
+							<div className="NoKTA">9922090922309201</div>
+							<div className="Jabatan">Presiden</div>
+							<div className="Wilayah">Kota Salatiga</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo2.jpg")})` }}>
+							</div>
+							<div className="Nama">Kimono Budi Fuzzy</div>
+							<div className="NoKTA">9922090922309201</div>
+							<div className="Jabatan">Presiden</div>
+							<div className="Wilayah">Kota Salatiga</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo3.jpg")})` }}>
+							</div>
+							<div className="Nama">Kimono Budi Fuzzy</div>
+							<div className="NoKTA">9922090922309201</div>
+							<div className="Jabatan">Presiden</div>
+							<div className="Wilayah">Kota Salatiga</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo1.jpg")})` }}>
+							</div>
+							<div className="Nama">Kimono Budi Fuzzy</div>
+							<div className="NoKTA">9922090922309201</div>
+							<div className="Jabatan">Presiden</div>
+							<div className="Wilayah">Kota Salatiga</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="BoxOverlay">
+								<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo2.jpg")})` }}>
+								</div>
+								<div className="Nama">Kimono Budi Fuzzy</div>
+								<div className="NoKTA">9922090922309201</div>
+								<div className="Jabatan">Presiden</div>
+								<div className="Wilayah">Kota Salatiga</div>
+								</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="BoxOverlay">
+								<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo3.jpg")})` }}>
+								</div>
+								<div className="Nama">Kimono Budi Fuzzy</div>
+								<div className="NoKTA">9922090922309201</div>
+								<div className="Jabatan">Presiden</div>
+								<div className="Wilayah">Kota Salatiga</div>
+							</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="BoxOverlay">
+								<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo1.jpg")})` }}>
+								</div>
+								<div className="Nama">Kimono Budi Fuzzy</div>
+								<div className="NoKTA">9922090922309201</div>
+								<div className="Jabatan">Presiden</div>
+								<div className="Wilayah">Kota Salatiga</div>
+								</div>
+						</div>
+					</div>
+					<div className="column-l-3 SingleDataAnggota">
+						<div className="BoxSingleDataAnggota">
+							<div className="BoxOverlay">
+								<div className="Foto" style={{ backgroundImage: `url(${require("../../photos/photo3.jpg")})` }}>
+								</div>
+								<div className="Nama">Kimono Budi Fuzzy</div>
+								<div className="NoKTA">9922090922309201</div>
+								<div className="Jabatan">Presiden</div>
+								<div className="Wilayah">Kota Salatiga</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/contents/Anggota/SearchAnggota.js
+++ src/contents/Anggota/SearchAnggota.js
@@ -9,17 +9,17 @@ class FastSearch extends React.Component {
     return (
       <form className="FastSearch">
         <div className="InputWrapper">
-          <input type="text" name="search" id="input-item-search" placeholder="ex: Hoodie Zipper" />
+          <input type="text" name="search" id="input-item-search" placeholder="Contoh: Riettsruff" />
         </div>
         <div className="buttonWrapper row no-gutters">
           <div className="SingleButtonWrapper column-6">
-            <button type="button" class="SearchButton">
+            <button type="button" className="SearchButton">
               <FontAwesomeIcon icon={faSearch} />
               <span>Cari</span>
             </button>
           </div>
           <div className="SingleButtonWrapper column-6">
-            <button type="button" class="ClearButton">
+            <button type="button" className="ClearButton">
               <FontAwesomeIcon icon={faTimes} />
               <span>Bersihkan</span>
             </button>
